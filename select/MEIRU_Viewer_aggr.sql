ba203dc88ce425f976344f5716bfb39d2a9b7546
alipompita <58941738+alipompita@users.noreply.github.com>
2025-09-10 15:11:06 +0200

    add select sql

---
+++ select/MEIRU_Viewer_aggr.sql
@@ -0,0 +1,41 @@
+show databases;
+
+create database if not exists meiru_viewer_aggr;
+
+use meiru_viewer_aggr;
+
+create table if not exists sites(
+	id int not null primary key,
+    name varchar(16) not null unique,
+    is_urban boolean
+);
+insert into sites values (1, 'Karonga', false),(2, 'Lilongwe', true);
+
+create table if not exists dss_members(
+	ident varchar(8) not null primary key,
+    sex int,
+    dob date,
+    age int,
+    site int,
+		foreign key (site) references sites (id)
+);
+
+drop table if exists study_screen_points;
+create table if not exists study_screen_points(
+	code varchar(8) not null primary key,
+    study varchar(4),
+    description text
+);
+
+create table if not exists screens(
+	id int not null primary key auto_increment,
+    source varchar(8) not null,
+		foreign key (source) references study_screen_points (code),
+	ident varchar(8) not null,
+		foreign key (ident) references dss_members(ident),
+	stid varchar(8),
+    intdate date,
+    phq9tot int,
+    gad7tot int
+);
+
---
+++ select/MEIRU_Viewer_aggr_select.sql
@@ -0,0 +1,59 @@
+show tables;
+
+select member.sex, count(screen.ident) as anx_symptoms
+from screens screen 
+left join dss_members member on member.ident = screen.ident
+where gad7tot >= 9 and member.site = 2
+group by member.sex;
+
+select member.sex, count(screen.ident) as drp_symptoms
+from screens screen 
+left join dss_members member on member.ident = screen.ident
+where phq9tot >= 9 and member.site = 1
+group by member.sex;
+
+
+
+
+select distinct screen.ident, max(phq9tot)
+from screens screen
+left join dss_members member on member.ident = screen.ident
+where phq9tot >= 9
+group by ident;
+
+
+select date_format(intdate, '%Y') As year, count(ident) as anx_symptoms
+from screens
+where gad7tot >= 9
+group by date_format(intdate, '%Y');
+
+select
+case
+	when gad7tot between 0 and 4 then 'Minimal'
+    when gad7tot between 5 and 9 then 'Mild'
+    when gad7tot between 10 and 14 then 'Moderate'
+    when gad7tot between 15 and 21 then 'Severe'
+    else 'unkown'
+end as severity,
+count(ident)
+from screens
+group by severity;
+
+select count(id) from screens;
+
+select source, count(ident) from screens group by source;
+
+select ident, count(*) as screens
+from screens 
+group by ident
+having count(*) > 1
+order by screens desc;
+
+-- idents with more than one screen 
+select count(ident)
+from screens
+where ident in (select ident
+from screens 
+group by ident
+having count(*) > 1);
+
